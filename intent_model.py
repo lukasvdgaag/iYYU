bec3e4b8ae5215251d1474cbedd85cfab3d527a7
FrankVanDerVelde <frankvdv1998@gmail.com>
2023-05-17 21:42:45 +0200

    changes

--- intent_model.py
+++ intent_model.py
@@ -1,133 +1,74 @@
 import json
 import os
 import torch
-from transformers import BertForSequenceClassification, BertTokenizer
+from transformers import BertForSequenceClassification, BertTokenizer, AdamW
+from torch.utils.data import Dataset, DataLoader
 
 class IntentModel:
-
-    def __init__(self,  train_data, label_map, labels):
+    def __init__(self,  train_data, label_map):
         self.train_data = train_data
         self.label_map = label_map
-        self.labels = labels
         self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
-        self.model = torch.load("trained_bert.pth") if os.path.exists("trained_bert.pth") else self.train_model()
-        
+        self.model = self.load_model()
+
+    def load_model(self):
+        if os.path.exists("trained_bert.pth"):
+            return BertForSequenceClassification.from_pretrained("trained_bert.pth")
+        else:
+            return self.train_model()
 
     def train_model(self):
         # Set a random see for the models random number generator to ensure reproducibility
         torch.manual_seed(42)
 
         model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=len(self.label_map))
+
         # Tokenize the training data and convert to tensors
         inputs = self.tokenizer.batch_encode_plus([data[0] for data in self.train_data], padding=True, truncation=True, return_tensors="pt")
+        labels = torch.tensor([self.label_map[data[1]] for data in self.train_data])
+
+        dataset = IntentDataset(inputs, labels)
+        data_loader = DataLoader(dataset, batch_size=16, shuffle=True)
 
         # Fine-tune the model on the training data
-        optimizer = torch.optim.Adam(model.parameters(), lr=5e-4) #default 5e-5
-        loss_fn = torch.nn.CrossEntropyLoss()
-        for epoch in range(20):
-            optimizer.zero_grad()
-            outputs = model(inputs["input_ids"], attention_mask=inputs["attention_mask"], labels=self.labels)
-            loss = outputs.loss
-            loss.backward()
-            optimizer.step()
+        optimizer = AdamW(model.parameters(), lr=5e-4) #default 5e-5
+        num_epochs = 20
+
+        for epoch in range(num_epochs):
+            for batch in data_loader:
+                outputs = model(**batch)
+                loss = outputs.loss
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
 
             print(f"Epoch {epoch+1}, Loss: {loss.item()}")
 
-            # Evaluate the model on the training data
-            predictions = outputs.logits.argmax(axis=1)
-            accuracy = (predictions == self.labels).sum()
-        
         # Save the model
-        torch.save(model, "trained_bert.pth")
+        model.save_pretrained("trained_bert.pth")
 
         return model
-        
-    def get_model(self):
-        return self.model
-    
-    def get_tokenizer(self):
-        return self.tokenizer
 
     def get_intent(self, question):
-        # Tokenize the test question and convert to tensors
+        # Tokenize the question and convert to tensors
         inputs = self.tokenizer.encode_plus(question, padding=True, truncation=True, return_tensors="pt")
 
-        # Get the model's prediction for the test question
+        # Get the model's prediction for the question
         with torch.no_grad():
-            outputs = self.model(inputs["input_ids"], attention_mask=inputs["attention_mask"])
-        predictions = outputs.logits.argmax(axis=1)
-        predicted_label = list(self.label_map.keys())[list(self.label_map.values()).index(predictions[0].item())]
-        # print(f"Predicted intent: {predicted_label}")
+            outputs = self.model(**inputs)
+        predicted_label = list(self.label_map.keys())[list(self.label_map.values()).index(outputs.logits.argmax(axis=1)[0].item())]
         return predicted_label
 
+class IntentDataset(Dataset):
+    def __init__(self, inputs, labels):
+        self.inputs = inputs
+        self.labels = labels
 
-    def evaluate_model(self, test_data):
-        results = []
+    def __len__(self):
+        return len(self.inputs["input_ids"])
 
-        total_document_correct_counts = 0
-        total_document_counts = 0
-
-        for data in test_data:
-            correct_intent = data["intent"]
-
-            questions_results = {
-                "correct_intent": correct_intent,
-                "train_questions": [],
-                "test_questions": [],
-                "test_questions_advanced": []
-            }
-
-            total_correct_counts = 0
-            total_counts = 0
-
-            # Loop over the question keys
-            for key in ["train_questions", "test_questions", "test_questions_advanced"]:
-                correct_count = 0
-
-                # Check if questions array exists
-                if key in data:
-                    questions = data[key]
-                    total_count = len(questions)
-
-                    for question in questions:
-                        predicted_intent = self.get_intent(question)
-                        correctly_predicted = (predicted_intent == correct_intent)
-                        if correctly_predicted:
-                            correct_count += 1
-                        question_result = {
-                            "question": question,
-                            "predicted_intent": predicted_intent,
-                            "intent_correctly_predicted": correctly_predicted
-                        }
-
-                        questions_results[key].append(question_result)
-
-                    questions_results[key + "_results"] = f"{correct_count} out of {total_count} correct"
-                    total_correct_counts += correct_count
-                    total_counts += total_count
-
-            questions_results["total_correctness"] = round((total_correct_counts / total_counts) * 100, 2)
-
-            results.append(questions_results)
-
-            total_document_correct_counts += total_correct_counts
-            total_document_counts += total_counts
-
-        total_document_correctness = round((total_document_correct_counts / total_document_counts) * 100, 2)
-
-        # Create a combined result object
-        combined_result = {
-            "results": results,
-            "total_correct_counts": total_document_correct_counts,
-            "total_counts": total_document_counts,
-            "total_correctness": total_document_correctness
-        }
-
-        # Save results to a JSON file
-        with open('evaluation_results.json', 'w') as outfile:
-            json.dump(combined_result, outfile, indent=4)
-
-        return combined_result
-
-
+    def __getitem__(self, idx):
+        item = {key: val[idx] for key, val in self.inputs.items()}
+        item["labels"] = self.labels[idx]
+        return item
