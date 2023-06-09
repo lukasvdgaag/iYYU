90ba58f6a9700bda0a78d3cc338489375fef3f53
Lukas van der Gaag <lukasvdgaag@gmail.com>
2023-06-09 15:30:01 +0200

    Security prediction (#23)
    * intents for the problematic code
    
    * small cahnges
    
    * settings beginning
    
    * damn it didn't cooperate at all
    
    * added intent recognition to method
    
    * working on bot response
    
    * showing the 3 questions
    
    * change privacy level
    
    * Trying to ad points
    
    * try to add points
    
    * Revert "try to add points"
    
    This reverts commit b419e596152f4516db353a587a7e5351e0999b1d.
    
    * correction push
    
    * response
    
    * new
    
    * before trying to fix bugs
    
    * some of the bugs fixed
    
    * Finalized security questionaire, readded delays.
    
    Co-authored-by: JustZineddine <60385452+JustZineddine@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Milos Mihajlovic <Milos.mihajlovic@hva.nl>
    Co-authored-by: JustZineddine <60385452+JustZineddine@users.noreply.github.com>

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,18 +1,55 @@
 import random
+import time
+from settings import Settings
+
 
 class ChatbotLogic:
-    def __init__(self, data, gpt_model, intent_model, minimum_confidence_score, privacy_level):
+    
+    def __init__(self, data, gpt_model, intent_model, minimum_confidence_score, privacy_level, give_suggestions):
         self.data = data
         self.gpt_model = gpt_model
         self.intent_model = intent_model
         self.minimum_confidence_score = minimum_confidence_score
         self.privacy_level = privacy_level
+        self.give_suggestions = give_suggestions
         self.history = []
         self.dialogue = {'active': False, 'counter': 0}
+        self.settings = Settings()
+
+        self.yes_keywords = [
+            "yes",
+            "yeah",
+            "yep",
+            "yup",
+            "sure",
+            "absolutely",
+            "definitely",
+            "certainly",
+            "ok",
+            "alright",
+            "agree",
+        ]
+
+        # a list of keywords that indicate a negative answer
+        self.no_keywords = [
+            "no",
+            "nope",
+            "nah",
+            "not",
+            "nay",
+            "negative",
+            "never",
+            "disagree",
+            "decline",
+            "deny",
+            "don't",
+        ]
+
 
         # Add welcome message
-        self.add_bot_response("Hello, I am the iYYU privacy bot. I am here to help you with your questions about the company. What can I help you with today?")
-        
+        self.add_bot_response(
+            "Hello, I am the iYYU privacy bot. I am here to help you with your questions about the company. What can I help you with today?", False)
+
     def get_object_by_intent(self, intent):
         for object in self.data:
             if object['intent'] == intent:
@@ -30,39 +67,77 @@ class ChatbotLogic:
 
     def bot(self, history):
         user_message = self.history[-1][0]
-        bot_response = self.determine_bot_response(user_message)
+        bot_response, add_delay = self.determine_bot_response(user_message)
 
-        self.add_bot_response(bot_response)
-
-        # if not bot_response.startswith("ChatGPT - "):
-        #     time.sleep(1)
+        self.add_bot_response(bot_response, delay=add_delay)
         return self.history
 
-    def add_bot_response(self, bot_response):
+    def add_bot_response(self, bot_response, delay=True):
+        if delay:
+            time.sleep(1)
+
         self.history.append([None, bot_response])
         return self.history
-        
+
+    def update_count(self, count):
+        question = self.settings.get_security_level_question(count)
+        self.dialogue.update({'active': True, 'counter': count})
+        return question
+
+    def answer_contains_word(self, user_message, keywords):
+        user_message = user_message.lower()
+        for word in keywords:
+            if word in user_message:
+                return True
+        return False
 
     def determine_bot_response(self, user_message):
+        ### Todo:
+        #yes_response and no_response maken, zodat de gebruiker op verschillende manier zijn beslissing kan aanduiden
         '''
         Condition numbers match up with the condition numbers in the chart
-        
         '''
+        step = self.dialogue['counter']
+        use_gpt = False
+
+
         # Condition 1: Is the dialogue active?
         if (self.dialogue['active']):
             '''
                 Use an array of questions to have a dialogue with the user and use the users answers
             '''
-            response = '(Test): Starting dialogue'
+            response = None
+            # 1 pakt counter value
 
-            # Condition 2: Are there dialogue questions left?
-            dialogue_questions = ['test', 'test2', 'test3']
-            if (self.dialogue['counter'] < len(dialogue_questions)):
-                response = '(Test): More dialogue questions'
+            number_of_questions = len(self.settings.get_all_security_level_questions())
+
+            
+            # niet de laatste vraag
+            if step < number_of_questions - 1:
+                
+                if self.answer_contains_word(user_message, self.yes_keywords):
+                    step += 1
+                    return self.update_count(step), True
+                elif self.answer_contains_word(user_message, self.no_keywords):
+                    step += 1
+                    self.settings.add_security_level_points(1)
+                    return self.update_count(step), True
+                else:
+                    return 'Please answer with yes or no.', True
             else:
-                response = '(Test): Ran out of dialogue questions, computing answer then awaiting user input'
+                # de laatste vraag
+                if self.answer_contains_word(user_message, self.yes_keywords):
+                    response = self.settings.generate_security_level_response(self.give_suggestions)
+                elif self.answer_contains_word(user_message, self.no_keywords):
+                    self.settings.add_security_level_points(1)  # Increment points for the final "no"
+                    response = self.settings.generate_security_level_response(self.give_suggestions)
+                else:
+                    return 'Please answer with yes or no.', True
+                
+                self.dialogue.update({"active": False, "counter": 0})
 
         else:
+            self.settings.points = 0
             print('2')
             intent_name, confidence_score = self.intent_model.get_intent(question=user_message)
             confidence_score = max(confidence_score)
@@ -73,14 +148,14 @@ class ChatbotLogic:
 
             # test implementation to test whether conversational dialogues work.
             if (user_message == 'lol'):
-                intent_name = 'initalize_user_settings'
+                intent_name = 'privacy_settings_response'
                 confidence_score = 1.0
 
             # Condition 3: Is the confidence score for the intent high enough to use it?
             if confidence_score < self.minimum_confidence_score:
                 print('3: model is not confident on the intent')
                 # Condition 4: Does the user have a high or low privacy level?
-                if self.privacy_level >= 1: 
+                if self.privacy_level > 1:
                     # tell user to rephrase
                     print('4: high privact level.')
                     response = "I'm sorry, I'm not sure I understand your question. Could you please rephrase it?"
@@ -88,15 +163,16 @@ class ChatbotLogic:
                     # send question to ChatGPT
                     print('4: low privacy level.')
                     response = self.gpt_model.answer_question(question=user_message)
+
             else:
                 print('3: model is confident!')
                 # Condition 5: Is there intent that requires a specific action?
-                if intent_name == 'initalize_user_settings':
+                if intent_name == 'privacy_settings_response':
                     # Logic to ask user questions to set the settings here
-                    response = '(Test): Specific intent, initialize settings'
+                    response = None
+                    self.add_bot_response('Sure, I can help you with that. I will ask you a few questions to determine your desired level of privacy.')
+                    return self.update_count(0), False
 
-                    self.dialogue.update({'active': True, 'counter': 0})
-                    self.determine_bot_response(user_message)
                 elif intent_name == 'change_setting':
                     # NER keywoard recognition implementation here
                     response = '(Test): Specific intent, change specific setting'
@@ -109,12 +185,11 @@ class ChatbotLogic:
                         response = random.choice(intent_data['responses'])
                     else:
                         print('6: ChatGPT should answer.')
-                        if self.privacy_level >= 1: 
+                        if self.privacy_level >= 1:
                             prompt_to_send = random.choice(intent_data['responses'])
                             response = self.gpt_model.answer_question(question=prompt_to_send)
                         else:
                             response = self.gpt_model.answer_question(question=user_message)
 
-
         # Finally return the response
-        return response
+        return response, not use_gpt
--- src/settings.py
+++ src/settings.py
@@ -5,9 +5,15 @@ class Settings:
     user_data_location = 'assets/user_data.json'
 
     def __init__(self):
+        self.user_id = '0'
+
         self.load_user_data()
         self.load_security_levels()
-        self.current_user = None
+
+        print(self.user_profiles)
+
+        self.current_user = self.user_profiles.get(self.user_id)
+        self.points = 0
 
         # print('User profiles:', self.user_profiles)
         # print('Low security:', self.low_security)
@@ -42,12 +48,12 @@ class Settings:
                 # Update the setting with the new state
                 user[setting_name] = setting_state
                 break
-        
+
         # Save the updated JSON data to file
         with open(self.user_data_location, 'w') as file:
             json.dump(data, file)
-    
-    
+
+
     def set_user_calling_card_visibility(self, user_id, individual_user_id, profile_card_component, component_state):
         '''
         This function should be called with the user_id and the second_user_id and the calling card visibility should be updated
@@ -74,121 +80,67 @@ class Settings:
 
         with open(self.user_data_location, 'w') as file:
             json.dump(data, file)
-        
-    def estimate_user_security_level(self):
-        '''
-        Set up this function to ask the user a bunch of questions, and then based on the answers choose a secutiry level that suits them
-        then use the above pre-defined user profile security levels to set their security settings
-        since this will be a rule based approach it should just be a bunch of if statements and use the gradio chat to communicate with the user
-        '''
-        
-        questions = [
+
+    def get_all_security_level_questions(self):
+        return [
             "Do you want your profile to be published?",
             "Do you want other users to be able to connect with you?",
             "Do you want your account to be visible for search?"
         ]
-        points = 0
 
-        for question in questions:
-            response = input(question + " (yes/no): ")
-            if response.lower() == "yes":
-                points += 0
-            else:
-                points += 1
+    def get_security_level_question(self, count):
+        # Implement the logic to retrieve a security level question based on the count
+        # Return the question string
+        # Example implementation:
+        questions = self.get_all_security_level_questions()
+        if count < len(questions):
+            return questions[count]
+        else:
+            return "No more questions available."
 
-        user_id = 0  # Change this based on the user's ID
-        self.current_user = self.user_profiles.get(user_id)
-        if self.current_user:
-            if points <= 1:
-                new_security_level = self.low_security
-            elif points == 2:
-                new_security_level = self.medium_security
-            else:
-                new_security_level = self.high_security
+    def add_security_level_points(self, points):
+        self.points += points
+        
+    def get_security_level_from_points(self, points):
+        if points <= 1:
+            return 1
+        elif points == 2:
+            return 2
+        else:
+            return 3
+        
+    def get_settings_from_security_level(self, level):
+        if level == 1:
+            return self.low_security
+        elif level == 2:
+            return self.medium_security
+        else:
+            return self.high_security
 
-            self.current_user['user_security_level'] = new_security_level
+    def generate_advice_response(self, level):
+        if level == 1:
+            return "You have chosen a low privacy level. Please go to your settings page and open Privacy settings. and make sure all the fields on privacy are turned on"
+        elif level == 2:
+            return "You have chosen a medium privacy level. Please go to your settings page and open Privacy settings. Turn off the option for all users to be able to find you, but keep the other options on"
+        else:
+            return "You have chosen a high privacy level. Please go to your settings page and open Privacy settings. Make sure all of the settings are turned off."
+
+    def generate_security_level_response(self, give_suggestions):
+        level = self.get_security_level_from_points(self.points)
+
+        if give_suggestions:
+            return self.generate_advice_response(level)
+
+        else:
+            new_settings = self.get_settings_from_security_level(level)
+            self.current_user.update(new_settings)
 
             with open(self.user_data_location, 'w') as file:
                 json.dump(list(self.user_profiles.values()), file)
 
-            return "User security level updated to: " + new_security_level
-        else:
-            return "User not found in user profiles."
-    
-    
-    # def set_user_calling_card_visibility(user_id, second_user_id):
-    #     None
-    #     with open('user_profiles.json', 'r') as file:
-    #     user_profiles = json.load(file)
-
-    # # Find the user by user_id
-    # user_index = None
-    # for i, user in enumerate(user_profiles):
-    #     if user['user_id'] == user_id:
-    #         user_index = i
-    #         break
-
-    # if user_index is None:
-    #     print("User not found.")
-    #     return
-
-    # # Find the user by second_user_id
-    # second_user_index = None
-    # for i, viewability in enumerate(user_profiles[user_index]['individual_user_viewability']):
-    #     if viewability['user_id'] == second_user_id:
-    #         second_user_index = i
-    #         break
-
-    # if second_user_index is None:
-    #     print("Second user not found.")
-    #     return
-
-    # visibility = input("Enter visibility (true/false) for the calling card: ")
-    # visibility = bool(visibility)
-
-    # # Update the visibility for the second_user_id
-    # user_profiles[user_index]['individual_user_viewability'][second_user_index]['profile_card_component_0_visible'] = visibility
-
-    # with open('user_profiles.json', 'w') as file:
-    #     json.dump(user_profiles, file, indent=4)
-    
-    # def handle_setting_questions(self) :
-        
-
-    #     return "test"
-
-
-    # def estimate_user_security_level(self):
-    #     if not self.current_user:
-    #         print("No current user set. Please set the current user using set_current_user(user_id).")
-    #         return None
-
-    #     begin_questions = [
-    #         "Do you want your profile to be high, medium, or low secured?",
-    #         "Do you want help with setting up your account settings?"
-    #     ]
-
-    #     answers = {}
-    #     security = self.low_security
-
-    #     for question in begin_questions:
-    #         answer = input(question + " ")
-    #         answers[question] = answer
-
-    #     permission_change_settings = answers["Do you want help with setting up your account settings?"]
-    #     if permission_change_settings.lower() == "no":
-    #         return security
-    #     else:
-    #         predefined_settings = answers["Do you want your profile to be high, medium, or low secured?"]
-    #         if predefined_settings.lower() == "high":
-    #             security = self.high_security
-    #         elif predefined_settings.lower() == "medium":
-    #             security = self.medium_security
-
-    #     return security
-
-    # def set_current_user(self, user_id):
-    #     for user in self.user_profiles:
-    #         if user['user_id'] == user_id:
-    #             self.current_user = user
-    #             break
+            if level == 1:
+                return "You have chosen a low privacy level. We changed your settings to your preferred combination"
+            elif level == 2:
+                return "You have chosen a medium privacy level. We changed your settings to your preferred combination"
+            else:
+                return "You have chosen a high privacy level. We changed your settings to your preferred combination"
